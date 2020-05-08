768a1e206858c2be6de05668f9e7bdc9c008e7aa
Héctor Salinas <hectorsalinas@MacBook-Pro-de-Osiris.local>
2020-05-08 12:45:16 -0500

    Added Counter

--- index.js
+++ index.js
@@ -26,12 +26,17 @@ io.on('connection', (socket) => {
       return callback(error);
     }
 
-    socket.emit('message', { user: '', text: `${user.name} connected`});
-    socket.broadcast.to(user.room).emit('message', { user: '', text: `${user.name} has joined.` });
+    socket.emit('message', { user: '', text: `${user.name} connected` });
+    socket.broadcast
+      .to(user.room)
+      .emit('message', { user: '', text: `${user.name} has joined.` });
 
     socket.join(user.room);
 
-    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room)
+    });
 
     callback();
   });
@@ -39,8 +44,21 @@ io.on('connection', (socket) => {
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id);
 
-    io.to(user.room).emit('message', { user: user.name, text: message});
-    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+    io.to(user.room).emit('message', { user: user.name, text: message });
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room)
+    });
+
+    callback();
+  });
+
+  //Counter
+  socket.on('increaseCounter', (message, callback) => {
+    let value = message + 1;
+    console.log(value);
+
+    io.emit('counterChanged', { number: value });
 
     callback();
   });
@@ -49,8 +67,14 @@ io.on('connection', (socket) => {
     const user = removeUser(socket.id);
 
     if (user) {
-      io.to(user.romm).emit('message', { user: '', text: `${ user.name } has left.` });
-      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+      io.to(user.romm).emit('message', {
+        user: '',
+        text: `${user.name} has left.`
+      });
+      io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+      });
     }
   });
 });
